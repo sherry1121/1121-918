f5d83398f35a11701eaddf34c9c95499e9ad83c0
sherry1121 <amy29619@gmail.com>
2017-09-15 15:11:17 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("學號：960101761\n系級：行動商務與多媒體應用學系一年級\n姓名：蔡咕咕\n時間：2016/03/04\n地點：\"I319\"\n");
+    }
+}
+
